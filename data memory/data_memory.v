c462f662503e74983fe2573cdf9556541e9c6b3b
dhmudalige <e18227@eng.pdn.ac.lk>
2023-04-10 21:31:16 +0530

    updated data_memory.v
    defined READ_DELAY and WRITE_DELAY

--- data memory/data_memory.v
+++ data memory/data_memory.v
@@ -11,6 +11,10 @@ module data_memory(
     readdata,
 	busywait
 );
+
+`define READ_DELAY #40
+`define WRITE_DELAY #40
+
 input				clock;
 input           	reset;
 input           	read;
@@ -36,50 +40,50 @@ end
 //Reading & writing
 always @(posedge clock) begin
 	if(readaccess) begin
-		readdata[7:0]   = #40 memory_array[{address,4'b0000}];
-		readdata[15:8]  = #40 memory_array[{address,4'b0001}];
-		readdata[23:16] = #40 memory_array[{address,4'b0010}];
-		readdata[31:24] = #40 memory_array[{address,4'b0011}];
+		readdata[7:0]   = `READ_DELAY memory_array[{address,4'b0000}];
+		readdata[15:8]  = `READ_DELAY memory_array[{address,4'b0001}];
+		readdata[23:16] = `READ_DELAY memory_array[{address,4'b0010}];
+		readdata[31:24] = `READ_DELAY memory_array[{address,4'b0011}];
 
-        readdata[39:32] = #40 memory_array[{address,4'b0100}];
-		readdata[47:40] = #40 memory_array[{address,4'b0101}];
-		readdata[55:48] = #40 memory_array[{address,4'b0110}];
-		readdata[63:56] = #40 memory_array[{address,4'b0111}];
+        readdata[39:32] = `READ_DELAY memory_array[{address,4'b0100}];
+		readdata[47:40] = `READ_DELAY memory_array[{address,4'b0101}];
+		readdata[55:48] = `READ_DELAY memory_array[{address,4'b0110}];
+		readdata[63:56] = `READ_DELAY memory_array[{address,4'b0111}];
 
-        readdata[71:64] = #40 memory_array[{address,4'b1000}];
-		readdata[79:72] = #40 memory_array[{address,4'b1001}];
-		readdata[87:80] = #40 memory_array[{address,4'b1010}];
-		readdata[95:88] = #40 memory_array[{address,4'b1011}];
+        readdata[71:64] = `READ_DELAY memory_array[{address,4'b1000}];
+		readdata[79:72] = `READ_DELAY memory_array[{address,4'b1001}];
+		readdata[87:80] = `READ_DELAY memory_array[{address,4'b1010}];
+		readdata[95:88] = `READ_DELAY memory_array[{address,4'b1011}];
 
-        readdata[103:96]  = #40 memory_array[{address,4'b1100}];
-		readdata[111:104] = #40 memory_array[{address,4'b1101}];
-		readdata[119:112] = #40 memory_array[{address,4'b1110}];
-		readdata[127:120] = #40 memory_array[{address,4'b1111}];
+        readdata[103:96]  = `READ_DELAY memory_array[{address,4'b1100}];
+		readdata[111:104] = `READ_DELAY memory_array[{address,4'b1101}];
+		readdata[119:112] = `READ_DELAY memory_array[{address,4'b1110}];
+		readdata[127:120] = `READ_DELAY memory_array[{address,4'b1111}];
 		
 		busywait = 0;
 		readaccess = 0;
 	end
 
 	if(writeaccess) begin
-		memory_array[{address,4'b0000}] = #40 writedata[7:0];
-		memory_array[{address,4'b0001}] = #40 writedata[15:8];
-		memory_array[{address,4'b0010}] = #40 writedata[23:16];
-		memory_array[{address,4'b0011}] = #40 writedata[31:24];
+		memory_array[{address,4'b0000}] = `WRITE_DELAY writedata[7:0];
+		memory_array[{address,4'b0001}] = `WRITE_DELAY writedata[15:8];
+		memory_array[{address,4'b0010}] = `WRITE_DELAY writedata[23:16];
+		memory_array[{address,4'b0011}] = `WRITE_DELAY writedata[31:24];
 		
-		memory_array[{address,4'b0100}] = #40 writedata[39:32];
-		memory_array[{address,4'b0101}] = #40 writedata[47:40];
-		memory_array[{address,4'b0110}] = #40 writedata[55:48];
-		memory_array[{address,4'b0111}] = #40 writedata[63:56];
+		memory_array[{address,4'b0100}] = `WRITE_DELAY writedata[39:32];
+		memory_array[{address,4'b0101}] = `WRITE_DELAY writedata[47:40];
+		memory_array[{address,4'b0110}] = `WRITE_DELAY writedata[55:48];
+		memory_array[{address,4'b0111}] = `WRITE_DELAY writedata[63:56];
 
-		memory_array[{address,4'b1000}] = #40 writedata[71:64];
-		memory_array[{address,4'b1001}] = #40 writedata[79:72];
-		memory_array[{address,4'b1010}] = #40 writedata[87:80];
-		memory_array[{address,4'b1011}] = #40 writedata[95:88];
+		memory_array[{address,4'b1000}] = `WRITE_DELAY writedata[71:64];
+		memory_array[{address,4'b1001}] = `WRITE_DELAY writedata[79:72];
+		memory_array[{address,4'b1010}] = `WRITE_DELAY writedata[87:80];
+		memory_array[{address,4'b1011}] = `WRITE_DELAY writedata[95:88];
 
-		memory_array[{address,4'b1100}] = #40 writedata[103:96];
-		memory_array[{address,4'b1101}] = #40 writedata[111:104];
-		memory_array[{address,4'b1110}] = #40 writedata[119:112];
-		memory_array[{address,4'b1111}] = #40 writedata[127:120];
+		memory_array[{address,4'b1100}] = `WRITE_DELAY writedata[103:96];
+		memory_array[{address,4'b1101}] = `WRITE_DELAY writedata[111:104];
+		memory_array[{address,4'b1110}] = `WRITE_DELAY writedata[119:112];
+		memory_array[{address,4'b1111}] = `WRITE_DELAY writedata[127:120];
 
 		busywait = 0;
 		writeaccess = 0;
